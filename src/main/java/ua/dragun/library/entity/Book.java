a81f0ed604e4c0898edb6afab3343e06de88040d
maksdragun <MDragunWork@gmail.com>
2018-08-30 18:03:35 +0300

    #02 add properties and entity

---
+++ src/main/java/ua/dragun/library/entity/Book.java
@@ -0,0 +1,53 @@
+package ua.dragun.library.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="library")
+public class Book {
+    private String id;
+    private String author;
+    private String title;
+    private String description;
+
+    public Book() {
+    }
+
+    public Book(String id, String author, String title, String description) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
